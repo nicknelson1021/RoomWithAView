70fc7e096551246ab56b8811ed57929e4acf3a4d
Nick <nicknelson1021@gmail.com>
2018-12-01 14:04:24 -0600

    Updating progress...

---
+++ app/src/main/java/org/nicknelson/roomwithaview/WordDao.java
@@ -0,0 +1,22 @@
+package org.nicknelson.roomwithaview;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface WordDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(WordEntity word);
+
+    @Query("DELETE FROM WordEntity")
+    void deleteAll();
+
+    @Query("SELECT * from WordEntity ORDER BY mWord ASC")
+    List<WordEntity> getAllWords();
+
+}
